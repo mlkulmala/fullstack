73db967c21e03a9ece5482aceee2c386d16143c0
mlkulmala <mlkulmala@hotmail.com>
2022-02-02 10:48:48 +0200

    Update App.js

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -33,7 +33,7 @@ const Statistics = ({ good, neutral, bad }) => {
 
 
 
-function Button({ handleClick, text }) {
+function Button ({ handleClick, text }) {
   return (
     <button onClick={handleClick}>
       {text}
